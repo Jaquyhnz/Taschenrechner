1e9579f1594212e303aab68fc59b7c0b2616fc23
Jaquyhnz <jaqulineh@schule.koeln>
2019-09-13 13:31:03 +0200

    Addieren mit zwei Zahlen

--- Taschenrechner_Henze/Taschenrechner_Henze/Program.cs
+++ Taschenrechner_Henze/Taschenrechner_Henze/Program.cs
@@ -10,6 +10,18 @@ namespace Taschenrechner_Henze
     {
         static void Main(string[] args)
         {
+            Console.Write("Schreib eine ganze Zahl: ");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Schreibe eine zweite ganze Zahl: ");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            int ergebnis = zahl1 + zahl2;
+
+            Console.WriteLine(ergebnis);
+            Console.ReadLine();
+
+
         }
     }
 }
