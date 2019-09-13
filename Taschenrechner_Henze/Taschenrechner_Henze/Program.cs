da1699d33e7d3d6e78194b9995e3da1e2dcbc9e8
Jaquyhnz <jaqulineh@schule.koeln>
2019-09-13 13:42:57 +0200

    Subtraktion zwei ganzer Zahlen

--- Taschenrechner_Henze/Taschenrechner_Henze/Program.cs
+++ Taschenrechner_Henze/Taschenrechner_Henze/Program.cs
@@ -10,18 +10,48 @@ namespace Taschenrechner_Henze
     {
         static void Main(string[] args)
         {
-            Console.Write("Schreib eine ganze Zahl: ");
-            int zahl1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Schreibe eine zweite ganze Zahl: ");
-            int zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1 fuer plus , 2 fuer minus , 3 fuer mal , 4 fuer geteilt");
+            int auswahl = Convert.ToInt32(Console.ReadLine());
 
-            int ergebnis = zahl1 + zahl2;
-
-            Console.WriteLine(ergebnis);
-            Console.ReadLine();
+            if (auswahl == 1)
+            {
+                Console.Clear();
 
 
+
+                Console.Write("Schreib eine ganze Zahl: ");
+                int addzahl1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Schreibe eine zweite ganze Zahl: ");
+                int addzahl2 = Convert.ToInt32(Console.ReadLine());
+
+                int addergebnis = addzahl1 + addzahl2;
+
+                Console.WriteLine(addergebnis);
+                Console.ReadLine();
+            }
+            else if (auswahl == 2)
+            {
+
+
+
+                Console.Clear();
+
+
+                Console.Write("Schreib eine ganze Zahl: ");
+                int subzahl1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Schreibe eine zweite ganze Zahl: ");
+                int subzahl2 = Convert.ToInt32(Console.ReadLine());
+
+                int subergebnis = subzahl1 - subzahl2;
+
+                Console.WriteLine(subergebnis);
+                Console.ReadLine();
+            }
+
+            Console.Clear();
         }
     }
 }
